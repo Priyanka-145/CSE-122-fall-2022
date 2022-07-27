8bf408838198f8ecb069ce290ba5644d3b74c098
Priyanka-145 <110048519+Priyanka-145@users.noreply.github.com>
2022-07-27 09:55:41 +0600

    Add files via upload

---
+++ lab 01/145-priya Way Too Long Words.cpp
@@ -0,0 +1,22 @@
+//Name: 145-priya
+//ID: 21225103145
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int n;
+
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+         string s;
+        cin>>s;
+        if (s.length()>10)
+       {
+           cout << s[0] << s.length()-2 << s[s.length()-1]<< endl;
+       }
+        else
+            cout<<s<<endl;
+    }
+}
